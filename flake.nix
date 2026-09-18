b8b8dccc103d1efb61d386e81fe29a3a43f86c97
Shantanu Singh <shsingh@broadinstitute.org>
2026-09-17 22:26:12 -0400

    Reapply "Package msgvault from a pinned source revision"
    This reverts commit 7fad6bc9d779e32a94c9242c04ce89113344ac6c.

--- flake.nix
+++ flake.nix
@@ -27,8 +27,8 @@
       flake = false;
     };
     msgvault = {
-      url = "github:wesm/msgvault";
-      inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:kenn-io/msgvault/4e1aae8c2e48bf5062788c6007831e41ea66ef20";
+      flake = false;
     };
     private.url = "git+ssh://git@github.com/shntnu/nixos-config-private";
   };
@@ -109,6 +109,11 @@
     in
     {
       devShells = forAllSystems devShell;
+      packages = forAllSystems (system: {
+        msgvault = nixpkgs.legacyPackages.${system}.callPackage ./modules/shared/msgvault-package.nix {
+          msgvaultSrc = msgvault;
+        };
+      });
       apps = nixpkgs.lib.genAttrs [ "aarch64-darwin" ] mkDarwinApps;
 
       darwinConfigurations = {
